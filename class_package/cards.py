d9f4e99e881e4d470ed7f39ae6bc7c8f1b80bff5
Luis Cortez <elluis.codes@gmail.com>
2021-08-04 17:07:06 -0500

    created Deck()
    instantiates an instance of a deck compromised of 52 instances of a card class
    representing a standard deck of playing cards
    
    created Deck.shuffle()
    shuffles a created instance of a deck 'IN PLACE'

--- class_package/cards.py
+++ class_package/cards.py
@@ -1,31 +1,43 @@
+from random import shuffle
+
+
 class Card:
     """
     create an instance of a card class used to populate a Deck() class
     """
-    def __init__(self, name, rank, value):
+
+    def __init__(self, name, suite, value):
         """
 
-        :param name: name of card
-        :param rank: rank of card
+        :param suite: name of card
+        :param name: rank of card
         :param value: uses val{} to assign a value to each card
         """
 
         self.name = name
-        self.rank = rank
+        self.suite = suite
         self.value = value
 
     def __str__(self):
-        return f"[ {self.name} of {self.rank} ] :: value = {self.value}"
+        return f"[ {self.name} of {self.suite} ] :: value = {self.value}"
 
 
 class Deck():
 
     def __init__(self):
-
-        suite = ('Hearts', 'Spades', 'Diamonds', 'Clubs')
-        val = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
-               'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
-
+        """
+        :return: a deck consisting of 52 cards found in a standard deck of playing cards
+        """
+        suites = ('Hearts', 'Spades', 'Diamonds', 'Clubs')
+        values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
+                  'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+        # empty list to append new card classes too
         self.deck = []
+        # loop to populate each card with correct data supplied from suites( ... ) and values{ ... }
+        for suite in suites:
+            for name, value in values.items():
+                self.deck.append(Card(suite, name, value))
 
-        for
+    def shuffle_deck(self):
+        shuffle(self.deck)
+
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
+from class_package.cards import Deck
 
 if __name__ == '__main__':
 
+    new_deck = Deck()
+    new_deck.shuffle_deck()
+
+    for index in range(0, 52):
+        print(new_deck.deck[index])
 
