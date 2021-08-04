60da270cb2e50acd4e0fa2e49a861c1986555380
Luis Cortez <elluis.codes@gmail.com>
2021-08-04 16:26:54 -0500

    removed val from Card class moved value dictionary to Deck() to avoid repetition Deck() will call Card() with the three following attributes : name, rank, value
    adjusted Card() unit test to reflect change

--- class_package/cards.py
+++ class_package/cards.py
@@ -2,19 +2,30 @@ class Card:
     """
     create an instance of a card class used to populate a Deck() class
     """
-    def __init__(self, name, rank):
+    def __init__(self, name, rank, value):
         """
 
         :param name: name of card
         :param rank: rank of card
         :param value: uses val{} to assign a value to each card
         """
-        val = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
-               'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
 
         self.name = name
         self.rank = rank
-        self.value = val[name]
+        self.value = value
 
     def __str__(self):
         return f"[ {self.name} of {self.rank} ] :: value = {self.value}"
+
+
+class Deck():
+
+    def __init__(self):
+
+        suite = ('Hearts', 'Spades', 'Diamonds', 'Clubs')
+        val = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
+               'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+
+        self.deck = []
+
+        for
--- test/test_cards.py
+++ test/test_cards.py
@@ -6,7 +6,11 @@ from class_package.cards import Card
 class TestCard(TestCase):
 
     def test_card_string_generation(self):
-        new_card = Card('Queen', 'Hearts')
+        """
+        generate an instance of a card object
+        :return: return string and compare to expected result
+        """
+        new_card = Card('Queen', 'Hearts', 12)
         expected = "[ Queen of Hearts ] :: value = 12"
         result = str(new_card)
 
